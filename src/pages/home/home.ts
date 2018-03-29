c08bd2c6534d2356bfc660cc8d4af53310914727
Vmussato <vinicius.mussato@gmail.com>
2018-03-28 22:45:51 -0300

    create model car

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Carro } from '../../modelos/carro';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,7 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  public carros;
+  public carros: Carro[];
 
   constructor(public navCtrl: NavController) {
     this.carros = [
