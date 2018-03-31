7603e83b748a816cb49d44439edd8932181dcb45
Vmussato <vinicius.mussato@gmail.com>
2018-03-31 00:32:08 -0300

    Added list of acessory and toggles

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -10,14 +10,15 @@ import { Carro } from '../../modelos/carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios = [];
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
       this.carro = this.navParams.get('carroSelecionado');
+      this.acessorios = [
+        {nome: 'Freio ABS', preco: 800 },
+        {nome: 'Ar-condicionado', preco: 1000 },
+        {nome: 'MP3 Player', preco: 500 },
+      ];
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EscolhaPage');
-  }
-
 }
