288aebe53be6aa0ace702c09eae8fdbceb90bc30
Vmussato <vinicius.mussato@gmail.com>
2018-03-31 01:07:01 -0300

    Created acessory interface and made adjustments to totalPrice

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
+import { Acessorio } from '../../modelos/acessorio';
 
 @IonicPage()
 @Component({
@@ -10,15 +11,28 @@ import { Carro } from '../../modelos/carro';
 export class EscolhaPage {
 
   public carro: Carro;
-  public acessorios = [];
+  public acessorios : Acessorio[];
+  private _precoTotal: number;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
+
       this.carro = this.navParams.get('carroSelecionado');
+      this._precoTotal = this.carro.preco;
       this.acessorios = [
         {nome: 'Freio ABS', preco: 800 },
         {nome: 'Ar-condicionado', preco: 1000 },
         {nome: 'MP3 Player', preco: 500 },
       ];
+  }
+
+  atualizaTotal(ativado:boolean, acessorio: Acessorio) {
+    ativado ? 
+      this._precoTotal += acessorio.preco :
+      this._precoTotal -= acessorio.preco;
+  }
+
+  get precoTotal() {
+    return this._precoTotal;
   }
 }
