21d648cfe6e315e414697b8ef48a6b269a11c459
Vmussato <vinicius.mussato@gmail.com>
2018-04-01 23:13:08 -0300

    Added root navigation after agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
 import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -42,7 +43,11 @@ export class CadastroPage {
     this._alerta = this.alertCtrl.create({
       title: 'Aviso',
       buttons: [
-        { text: 'ok' }
+        { text: 'ok',
+          handler: () => {
+            this.navCtrl.setRoot(HomePage);
+          }
+        }
       ]
     })
 
