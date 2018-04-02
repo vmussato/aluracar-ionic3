fafaea4d6fc2f2cd53c5afeef13040df0de3758c
Vmussato <vinicius.mussato@gmail.com>
2018-04-01 22:14:19 -0300

    Added details to cadastroPage

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Carro } from '../../modelos/carro';
 
 @IonicPage()
 @Component({
@@ -8,8 +9,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastroPage {
 
+  public carro: Carro;
+  public precoTotal: number;
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
+      this.carro = this.navParams.get('carroSelecionado');
+      this.precoTotal = this.navParams.get('precoTotal');
   }
 
 }
