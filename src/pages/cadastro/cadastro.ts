56c1aa01d9efc87eb3d37d70fcf8d0bbcdfb6eb1
Vmussato <vinicius.mussato@gmail.com>
2018-04-01 22:28:05 -0300

    Added Cadastro form fields

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -12,10 +12,22 @@ export class CadastroPage {
   public carro: Carro;
   public precoTotal: number;
 
+  public nome: string = '';
+  public endereco: string = '';
+  public email: string = '';
+  public data: string = new Date().toISOString();
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
       this.carro = this.navParams.get('carroSelecionado');
       this.precoTotal = this.navParams.get('precoTotal');
+  }
+
+  agenda() {
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
   }
 
 }
